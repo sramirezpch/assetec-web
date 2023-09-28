decbbec5909227ee082cfb34d1362fc2a02220f9
Sergio Ramirez <45132050+sramirezpch@users.noreply.github.com>
2023-09-27 23:24:52 -0500

    Delete assetec-website-main directory

--- assetec-website-main/src/assets/index.ts
+++
@@ -1,2 +0,0 @@
-export { default as LogoAssetec } from "./logo-assetec.png";
-export { default as HeroImage } from "./hero-image.webp";
--- assetec-website-main/src/components/Button/Button.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import "./styles.css";
-
-const Button = () => {
-  return <button>Contáctanos</button>;
-};
-
-export default Button;
