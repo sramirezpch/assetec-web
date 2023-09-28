abb67c7c9afe578b9085b832bc72f7d1c1c2f316
Sergio Ramirez <45132050+sramirezpch@users.noreply.github.com>
2023-09-27 23:23:30 -0500

    Add files via upload

---
+++ assetec-website-main/src/components/Button/Button.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+import "./styles.css";
+
+const Button = () => {
+  return <button>Contáctanos</button>;
+};
+
+export default Button;
---
+++ assetec-website-main/src/components/Navbar/Menu/index.ts
@@ -0,0 +1,1 @@
+export { default as NavbarMenu } from "./Menu";
